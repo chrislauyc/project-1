918ea9db17632a6cbb57fde63e24a8d26b07930e
chrislyc1991 <chrislyc1991@gmail.com>
2015-07-09 00:06:26 +0800

    Counting Vowels
    It counts the number of each vowel in a sentence.

---
+++ CountVowels.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string word;
+	cout << "Please enter your sentence or word." << endl;
+	getline(cin, word);
+	int countA = 0;
+	int countE = 0;
+	int countI = 0;
+	int countO = 0;
+	int countU = 0;
+	for (auto s : word)
+	{
+		s = tolower(s);
+		switch (s)
+		{
+		case 'a':
+			countA++;
+			break;
+		case'e':
+			countE++;
+			break;
+		case'i':
+			countI++;
+			break;
+		case'o':
+			countO++;
+			break;
+		case'u':
+			countU++;
+			break;
+
+		}
+	}
+	cout << "Number of A(s): " << countA << endl;
+	cout << "Number of E(s): " << countE << endl;
+	cout << "Number of I(s): " << countI << endl;
+	cout << "Number of O(s): " << countO << endl;
+	cout << "Number of U(s): " << countU << endl;
+
+	system("pause()");
+	return 0;
+}
